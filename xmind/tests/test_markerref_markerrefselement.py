55c997e7b9220fcffdf65a674e584a925088c84c
mykola-pysarev <34965077+mykola-pysarev@users.noreply.github.com>
2018-01-13 00:14:52 +0200

    fixes from review comments

--- xmind/tests/test_markerref_markerrefselement.py
+++ xmind/tests/test_markerref_markerrefselement.py
@@ -10,7 +10,7 @@ class MarkerRefsElementTest(base.Base):
     LOGGER = logging.getLogger('MarkerRefsElementTest')
 
     def test_init_without_parameters(self):
-        """test that object of the class could be created without parameters and it has correct static attribute"""
+        """test that object of the class could be created with different number of parameters and it has correct static attribute"""
         with patch('xmind.core.mixin.WorkbookMixinElement.__init__') as WorkbookMixinElementMock:
             _test_object = MarkerRefsElement()
             WorkbookMixinElementMock.assert_called_with(None, None)
@@ -21,8 +21,11 @@ class MarkerRefsElementTest(base.Base):
             MarkerRefsElement('test', 2)
             WorkbookMixinElementMock.assert_called_with('test', 2)
 
-            with self.assertRaises(Exception):
-                MarkerRefsElement('test', 2, 3)
-                WorkbookMixinElementMock.assert_called_with('test', 2, 3)
+            MarkerRefsElement(node=None, ownerWorkbook=3)
+            WorkbookMixinElementMock.assert_called_with(None, 3)
 
-        self.assertEqual(_test_object.TAG_NAME, TAG_MARKERREFS )
+            with self.assertRaises(Exception):
+                MarkerRefsElement('test', 2, 4)
+
+            self.assertEqual(WorkbookMixinElementMock.call_count, 4)
+            self.assertEqual(_test_object.TAG_NAME, TAG_MARKERREFS )

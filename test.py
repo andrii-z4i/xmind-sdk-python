bf5b880488b1f2e40bb0e89a520d920e801b93af
mykola-pysarev <34965077+mykola-pysarev@users.noreply.github.com>
2018-01-19 23:24:56 +0100

    #32 added tests for const.py (#33)

--- test.py
+++ test.py
@@ -17,7 +17,8 @@ testModules = [
     'xmind.tests.test_notes_plain_notes',
     'xmind.tests.test_notes_note_content_element',
     'xmind.tests.test_position',
-    'xmind.tests.test_markerrefelement'
+    'xmind.tests.test_markerrefelement',
+    'xmind.tests.test_const',
 ]
 
 suite = unittest.TestSuite()

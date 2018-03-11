6511967aa4c6fe8921cdfd0d1035314c8139f599
vlad-shulika <vlad.shulika.95@gmail.com>
2018-03-11 19:28:45 +0200

    Refactoring 3

--- xmind/tests/create_xmind_file_from_json.py
+++ xmind/tests/create_xmind_file_from_json.py
@@ -2,9 +2,14 @@
 import xmind, json
 from xmind.core import const
 from xmind.core.topic import TopicElement
+from xmind.tests import logging_configuration as lc
 
+class CreateXmindFileFromJson():
 
-class CreateXmindFileFromJson:
+    def getLogger(self):
+        if not getattr(self, '_logger', None):
+            self._logger = lc.get_logger('CreateXmindFileFromJson')
+        return self._logger
 
     def __init__(self, xmind_filename, json_filename):
         self.DB_of_topics = {}
@@ -12,7 +17,7 @@ class CreateXmindFileFromJson:
         self.xmind_filename = xmind_filename
         self.json_data = json.load(open(json_filename))
 
-    def create_topics(self, topic, father_topic, sheet, topics_type=const.TOPIC_ATTACHED):
+    def create_topics(self, topic, parent_topic, sheet, topics_type=const.TOPIC_ATTACHED):
         if type(topic) == list:
             for i in topic:
                 t = TopicElement(ownerWorkbook=self.workbook)
@@ -20,7 +25,7 @@ class CreateXmindFileFromJson:
                 self.add_markers(t, i)
                 self.add_position(t, i)
                 self.add_attr_branch_folded(t, i)
-                father_topic.addSubTopic(t, topics_type=topics_type)
+                parent_topic.addSubTopic(t, topics_type=topics_type)
                 try:
                     if type(i["children"]["topics"]) == list:
                         self.create_topics(i["children"]["topics"][0]["topic"], t, sheet)
@@ -28,16 +33,16 @@ class CreateXmindFileFromJson:
                     else:
                         self.create_topics(i["children"]["topics"]["topic"], t, sheet)
                 except:
-                    print("the end of branch")
+                    self.getLogger().debug("the end of branch")
                 finally:
                     self.add_plain_notes(t, i)
                     self.DB_of_topics.update({i["-id"]: t})
         elif type(topic) == dict:
-            if not father_topic:
+            if not parent_topic:
                 t = sheet.getRootTopic()
             else:
                 t = TopicElement(ownerWorkbook=self.workbook)
-                father_topic.addSubTopic(t, topics_type=topics_type)
+                parent_topic.addSubTopic(t, topics_type=topics_type)
             self.add_title(t, topic)
             self.add_markers(t, topic)
             self.add_position(t, topic)
@@ -49,7 +54,7 @@ class CreateXmindFileFromJson:
                 else:
                     self.create_topics(topic["children"]["topics"]["topic"], t, sheet)
             except:
-                print("the end of branch")
+                self.getLogger().debug("the end of branch")
             finally:
                 self.add_plain_notes(t, topic)
                 self.DB_of_topics.update({topic["-id"]: t})
@@ -65,7 +70,7 @@ class CreateXmindFileFromJson:
                     rel = sheet.createRelationship(self.DB_of_topics[relationship["-end1"]].getID(), self.DB_of_topics[relationship["-end2"]].getID(), relationship["title"])
                     sheet.addRelationship(rel)
         except:
-            print("No relationships")
+            self.getLogger().debug("No relationships")
 
     def add_title(self, created_topic, topic_from_json):
         if type(topic_from_json["title"]) == dict:
@@ -81,26 +86,26 @@ class CreateXmindFileFromJson:
             else:
                 created_topic.addMarker(topic_from_json["marker-refs"]["marker-ref"]["-marker-id"])
         except:
-            print("No markers")
+            self.getLogger().debug("No markers")
 
     def add_plain_notes(self, created_topic, topic_from_json):
         try:
             created_topic.setPlainNotes(topic_from_json["notes"]["plain"])
         except:
-            print("No plain notes")
+            self.getLogger().debug("No plain notes")
 
     def add_position(self, created_topic, topic_from_json):
         try:
             created_topic.setPosition(topic_from_json["position"]["-svg:x"], topic_from_json["position"]["-svg:y"])
         except:
-            print("No position")
+            self.getLogger().debug("No position")
 
     def add_attr_branch_folded(self, created_topic, topic_from_json):
         try:
             if type(topic_from_json["-branch"]):
                 created_topic.setFolded()
         except:
-            print("No attribute branch")
+            self.getLogger().debug("No attribute branch")
 
     def create_xmind_file(self):
         sheet = self.workbook.getPrimarySheet()
--- xmind/tests/test_compare_xmind_file_with_etalon.py
+++ xmind/tests/test_compare_xmind_file_with_etalon.py
@@ -2,7 +2,6 @@
 import zipfile
 from xmind.tests import logging_configuration as lc
 from xmind.tests import base
-from unittest import TestCase
 from xmlscomparator.xml_diff import create_xml_diff_from_strings
 from xmlscomparator.comparators.type_comparator import TypeComparator
 from xmlscomparator.comparators.text_comparator import TextComparator
@@ -10,39 +9,39 @@ from xmlscomparator.comparators.attr_comparator_policy import AttrComparatorPoli
 from xmlscomparator.comparators.attr_comparator import AttrComparator
 from xmind.tests.create_xmind_file_from_json import CreateXmindFileFromJson
 
-class TestE2EOpen(base.Base):
+
+class TestE2ECreate(base.Base):
 
     def getLogger(self):
         if not getattr(self, '_logger', None):
             self._logger = lc.get_logger('TestE2EOpen')
         return self._logger
-''' comparing block '''
-obj = CreateXmindFileFromJson('test.xmind', 'test_file.json')
-obj.create_xmind_file()
 
-unarchived = zipfile.ZipFile('test.xmind', 'r')
-test_file_to_compare = unarchived.read(unarchived.namelist()[0])
+    def test_e2e_create(self):
+        obj = CreateXmindFileFromJson('test.xmind', 'xmind/tests/test_file.json')
+        obj.create_xmind_file()
 
-unarchived = zipfile.ZipFile('test_file.xmind', 'r')
-test_file = unarchived.read(unarchived.namelist()[1])
-print(test_file)
-print(test_file_to_compare)
+        unarchived = zipfile.ZipFile('test.xmind', 'r')
+        test_file_to_compare = unarchived.read(unarchived.namelist()[0])
 
-_type_comparator = TypeComparator()
-_text_comparator = TextComparator()
-_attr_comparator = AttrComparator("")
-_attr_policy = AttrComparatorPolicy()
-_attr_policy.add_attribute_name_to_skip_compare('svg:width')
-_attr_policy.add_attribute_name_to_compare('marker-id')
-_attr_policy.add_attribute_name_to_compare('type')
-_attr_comparator.set_attr_comparator_policy(_attr_policy)
-_attr_comparator.set_check_values(False)
-_text_comparator.set_next_comparator(_attr_comparator)
-_type_comparator.set_next_comparator(_text_comparator)
-_comparator = create_xml_diff_from_strings(test_file, test_file_to_compare)
-_comparator.set_comparator(_type_comparator)
-_comparator.add_types_to_skip('extensions')
-_comparator.add_types_to_skip('notes')
-_comparator.add_types_to_skip('control-points')
+        unarchived = zipfile.ZipFile('xmind/tests/test_file.xmind', 'r')
+        test_file = unarchived.read(unarchived.namelist()[1])
 
-print(_comparator.compare())
+        _type_comparator = TypeComparator()
+        _text_comparator = TextComparator()
+        _attr_comparator = AttrComparator("")
+        _attr_policy = AttrComparatorPolicy()
+        _attr_policy.add_attribute_name_to_skip_compare('svg:width')
+        _attr_policy.add_attribute_name_to_compare('marker-id')
+        _attr_policy.add_attribute_name_to_compare('type')
+        _attr_comparator.set_attr_comparator_policy(_attr_policy)
+        _attr_comparator.set_check_values(False)
+        _text_comparator.set_next_comparator(_attr_comparator)
+        _type_comparator.set_next_comparator(_text_comparator)
+        _comparator = create_xml_diff_from_strings(test_file, test_file_to_compare)
+        _comparator.set_comparator(_type_comparator)
+        _comparator.add_types_to_skip('extensions')
+        _comparator.add_types_to_skip('notes')
+        _comparator.add_types_to_skip('control-points')
+
+        self.assertTrue(_comparator.compare())
